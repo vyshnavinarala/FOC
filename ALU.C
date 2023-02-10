56b4af7022beb229bb4f2054bfeccc02ab7ddb8e
vyshnavinarala <124862174+vyshnavinarala@users.noreply.github.com>
2023-02-10 20:07:13 +0530

    Add files via upload

---
+++ ALU.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int a,b;
+clrscr();
+printf("enter the two values:");
+scanf("%d%d",&a,&b);
+printf("addition:%d",a+b);
+printf("\n subtraction:%d",a-b);
+printf("\nmultiplication:%d",a*b);
+getch();
+}
