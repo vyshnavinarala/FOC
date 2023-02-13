0a3b96a628c9001544a69923377db2deb071a137
vyshnavinarala <124862174+vyshnavinarala@users.noreply.github.com>
2023-02-13 14:58:17 +0530

    Create string length.c

---
+++ string length.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char str[20];
+printf("enter the string:");
+gets(str);
+printf("length of str is %d",strlen(str));
+getch();
+}
